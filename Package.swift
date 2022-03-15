46d3ccbc40d0f6ef1c314a46d45f5be27163020c
paulohgs <paulohgs@alu.ufc.br>
2022-03-15 11:10:20 -0300

    Criação da struct principal no arquivo Organizer

--- Package.swift
+++ Package.swift
@@ -5,16 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "script-organizador",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "script-organizador",
-            dependencies: []),
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]),
         .testTarget(
             name: "script-organizadorTests",
             dependencies: ["script-organizador"]),
---
+++ Sources/script-organizador/Organizer.swift
@@ -0,0 +1,16 @@
+import Foundation
+import ArgumentParser
+
+struct Organizer: ParsableCommand {
+    
+    @Argument(help: "path do diretorio com arquivos duplicados")
+    var path: String!
+    
+    func run() throws {
+        if path != nil, let url = URL(string: path) {
+            print(url)
+        } else {
+            print("Digite um caminho válido")
+        }
+    }
+}
